06c6ff834edc5ad4288a494c7a830f27ac9dfaee
Junxiang Chen <chenfrey920417@outlook.com>
2019-01-08 01:14:31 +0000

    Add description image
    TODO:
    - Add more ticker data.
    - Write comments.

--- DescriptionWindow.java
+++ DescriptionWindow.java
@@ -7,13 +7,14 @@ import javafx.stage.Stage;
 public class DescriptionWindow extends Stage {
     public DescriptionWindow() {
         int width = 720;
-        int height = 480;
+        int height = 405;
 
         StackPane descriptionPane = new StackPane();
         Image image = new Image("banner-sample.png", true);
         ImageView imageView = new ImageView();
         imageView.setImage(image);
         imageView.fitWidthProperty().bind(this.widthProperty());
+        imageView.fitHeightProperty().bind(this.heightProperty());
         imageView.setPreserveRatio(true);
 
         descriptionPane.getChildren().add(imageView);
